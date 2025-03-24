185a967f3d134dc38f2b6ed9bf7abfe2c20c4236
Bensome01 <minicrafter.log@gmail.com>
2025-03-23 19:29:22 -0700

    added separateMethodFromParameter into JavaMethod

--- dest/javaFile/JavaMethod.js
+++ dest/javaFile/JavaMethod.js
@@ -1,7 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeJavaMethod = void 0;
+exports.separateMethodFromParameter = exports.makeJavaMethod = void 0;
 const makeJavaMethod = (tokens) => {
     return {};
 };
 exports.makeJavaMethod = makeJavaMethod;
+const separateMethodFromParameter = (line) => {
+    const methodNameLocation = line.tokens.findIndex(token => /\(/.test(token));
+    if (methodNameLocation === -1) {
+        return line;
+    }
+    const targetToken = line.tokens.at(methodNameLocation);
+    const separateLocation = targetToken.indexOf("(");
+    const separatedTokens = [targetToken.slice(0, separateLocation), targetToken.slice(separateLocation)];
+    return { tokens: line.tokens.toSpliced(methodNameLocation, 0, ...separatedTokens), index: line.index };
+};
+exports.separateMethodFromParameter = separateMethodFromParameter;
--- src/javaFile/JavaMethod.ts
+++ src/javaFile/JavaMethod.ts
@@ -1,3 +1,5 @@
+import { TokenizedLine } from "../Parse/tokenizedLine";
+
 export type JavaMethod =
 {
     //
@@ -5,4 +7,19 @@ export type JavaMethod =
 
 export const makeJavaMethod = (tokens: string[]): JavaMethod => {
     return {};
+}
+
+export const separateMethodFromParameter = (line: TokenizedLine): TokenizedLine => {
+    const methodNameLocation: number = line.tokens.findIndex(token => /\(/.test(token));
+
+    if (methodNameLocation === -1)
+    {
+        return line;
+    }
+
+    const targetToken: string = line.tokens.at(methodNameLocation)!;
+    const separateLocation: number = targetToken.indexOf("(");
+    const separatedTokens: string[] = [targetToken.slice(0, separateLocation), targetToken.slice(separateLocation)];
+
+    return { tokens: line.tokens.toSpliced(methodNameLocation, 0, ...separatedTokens), index: line.index };
 }
