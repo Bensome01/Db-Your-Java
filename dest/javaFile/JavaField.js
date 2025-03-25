cd13fb776c09d251c2fbcb2073bf98b72d712448
Bensome01 <minicrafter.log@gmail.com>
2025-03-24 20:25:09 -0700

    added parameters to JavaField type and changed constructor to match

--- dest/javaFile/JavaField.js
+++ dest/javaFile/JavaField.js
@@ -1,12 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.findJavaFields = exports.makeJavaField = void 0;
+const tokens_1 = require("../Parse/tokens");
 const makeJavaField = (tokens) => {
-    const mutableTokens = [];
-    tokens.forEach(token => mutableTokens.push(token));
-    const fieldName = mutableTokens.pop();
-    const fieldType = mutableTokens.pop();
-    return { fieldName: fieldName, fieldType: fieldType, keywords: mutableTokens };
+    const indexOfAnnotationEnd = tokens
+        .findIndex(token => tokens_1.annotations
+        .some(annotation => token === annotation.source));
+    const annotationsEnd = indexOfAnnotationEnd === -1 ? 0 : indexOfAnnotationEnd;
+    const fieldAnnotations = tokens.slice(0, annotationsEnd);
+    const equalSignLocation = tokens.findIndex(token => token === "=");
+    const declarationEnd = equalSignLocation === -1 ? tokens.length : equalSignLocation;
+    return {
+        annotations: fieldAnnotations,
+        keywords: tokens.slice(annotationsEnd, declarationEnd - 2),
+        fieldType: tokens.at(declarationEnd - 2),
+        fieldName: tokens.at(declarationEnd - 1),
+        fieldValue: equalSignLocation === -1 ? "" : tokens.at(equalSignLocation + 1)
+    };
 };
 exports.makeJavaField = makeJavaField;
 const findJavaFields = (file) => {
--- src/javaFile/JavaField.ts
+++ src/javaFile/JavaField.ts
@@ -1,19 +1,32 @@
+import { annotations } from "../Parse/tokens";
 import { TokenizedLine } from "./../Parse/tokenizedLine";
 
 export type JavaField = 
 {
+    annotations: string[];
     keywords: string[];
-    fieldName: string;
     fieldType: string;
+    fieldName: string;
+    fieldValue: string;
 };
 
 export const makeJavaField = (tokens: string[]): JavaField => {
-    const mutableTokens: string[] = [];
-    tokens.forEach(token => mutableTokens.push(token));
-    const fieldName: string = mutableTokens.pop()!;
-    const fieldType: string = mutableTokens.pop()!;
+    const indexOfAnnotationEnd: number = tokens
+        .findIndex(token => annotations
+            .some(annotation => token === annotation.source));
+    const annotationsEnd: number = indexOfAnnotationEnd === -1 ? 0: indexOfAnnotationEnd;
 
-    return {fieldName: fieldName, fieldType: fieldType, keywords: mutableTokens};
+    const fieldAnnotations: string[] = tokens.slice(0, annotationsEnd);
+    const equalSignLocation: number = tokens.findIndex(token => token === "=");
+    const declarationEnd: number = equalSignLocation === -1 ? tokens.length : equalSignLocation
+
+    return {
+        annotations: fieldAnnotations,
+        keywords: tokens.slice(annotationsEnd, declarationEnd - 2),
+        fieldType: tokens.at(declarationEnd - 2)!,
+        fieldName: tokens.at(declarationEnd - 1)!,
+        fieldValue: equalSignLocation === -1 ? "" : tokens.at(equalSignLocation + 1)!
+    };
 }
 
 export const findJavaFields = (file: TokenizedLine[]): TokenizedLine[] => {
